775ee1abac10a652aa29c865e4558a0fd06d7c4a
Ben Fersenheim <benferse@microsoft.com>
2022-12-13 20:21:46 -0500

    Day 12

---
+++ src/day12.rs
@@ -0,0 +1,88 @@
+//! Day 12 - Hill climbing algorithm
+
+use std::collections::{HashSet, VecDeque};
+
+pub fn load_grid(lines: &[&str]) -> (Vec<Vec<u8>>, (isize, isize), (isize, isize)) {
+    let mut grid = vec![];
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+
+    for (u, row) in lines.iter().enumerate() {
+        let grid_col = row
+            .chars()
+            .enumerate()
+            .inspect(|(v, c)| {
+                if *c == 'S' {
+                    start = (*v as isize, u as isize);
+                } else if *c == 'E' {
+                    end = (*v as isize, u as isize);
+                }
+            })
+            .map(|(_, c)| {
+                match c {
+                    'S' => 'a' as u8,
+                    'E' => 'z' as u8,
+                    c => c as u8,
+                }
+            })
+            .collect();
+
+        grid.push(grid_col);
+    }
+
+    (grid, start, end)
+}
+
+pub fn do_bfs(grid: &Vec<Vec<u8>>, start: (isize, isize), end: (isize, isize)) -> u32 {
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+
+    queue.push_back((0, start));
+    visited.insert(start);
+
+    while let Some((distance, current)) = queue.pop_front() {
+        let (cur_x, cur_y) = current;
+        for (x, y) in [(cur_x - 1, cur_y), (cur_x + 1, cur_y), (cur_x, cur_y - 1), (cur_x, cur_y + 1)] {
+            let ux = x as usize;
+            let uy = y as usize;
+
+            if x >= 0 && y >= 0 && uy < grid.len() && ux < grid[uy].len() && grid[uy][ux] <= grid[cur_y as usize][cur_x as usize] + 1 && visited.insert((x, y)) {
+                if (x, y) == end {
+                    return distance + 1;
+                }
+
+                queue.push_back((distance + 1, (x, y)));              
+            }
+        }
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod answers {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(SAMPLE_GRID => 31; "problem 1 with example data")]
+    #[test_case(personal_grid().as_slice() => 534; "problem 1 with personal data")]
+    fn run_test(input: &[&str]) -> u32 {
+        let (mut grid, start, end) = load_grid(input);
+        do_bfs(&mut grid, start, end)
+    }
+
+    const SAMPLE_GRID: &[&str] = &[
+        "Sabqponm",
+        "abcryxxl",
+        "accszExk",
+        "acctuvwj",
+        "abdefghi",
+    ];
+
+    fn personal_grid() -> Vec<&'static str> {
+        include_str!("./input/day12.txt")
+            .lines()
+            .map(str::trim)
+            .collect()
+    }
+}
